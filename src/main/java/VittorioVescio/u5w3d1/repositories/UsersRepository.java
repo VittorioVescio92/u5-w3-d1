ee1b486c1466ead31b6703dc916cd1749ec58586
VittorioVescio92 <123975369+VittorioVescio92@users.noreply.github.com>
2023-06-12 16:52:36 +0200

    +

---
+++ src/main/java/VittorioVescio/u5w3d1/repositories/UsersRepository.java
@@ -0,0 +1,14 @@
+package VittorioVescio.u5w3d1.repositories;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import VittorioVescio.u5w3d1.entities.User;
+
+@Repository
+public interface UsersRepository extends JpaRepository<User, UUID> {
+	Optional<User> findByEmail(String email);
+}
---
+++ src/main/java/VittorioVescio/u5w3d1/services/UsersService.java
@@ -0,0 +1,37 @@
+package VittorioVescio.u5w3d1.services;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import VittorioVescio.u5w3d1.entities.User;
+import VittorioVescio.u5w3d1.entities.payloads.UserRegistrationPayload;
+import VittorioVescio.u5w3d1.exceptions.BadRequestException;
+import VittorioVescio.u5w3d1.exceptions.NotFoundException;
+import VittorioVescio.u5w3d1.repositories.UsersRepository;
+
+@Service
+public class UsersService {
+	@Autowired
+	UsersRepository usersRepo;
+
+	public User findById(UUID id) throws NotFoundException {
+		return usersRepo.findById(id).orElseThrow(() -> new NotFoundException("Utente non trovato!"));
+
+	}
+
+	public User create(UserRegistrationPayload u) {
+
+		usersRepo.findByEmail(u.getEmail()).ifPresent(user -> {
+			throw new BadRequestException("Email " + user.getEmail() + " already in use!");
+		});
+		User newUser = new User(u.getUsername(), u.getNome(), u.getEmail(), u.getPassword());
+		return usersRepo.save(newUser);
+	}
+
+	public User findByEmail(String email) throws NotFoundException {
+		return usersRepo.findByEmail(email)
+				.orElseThrow(() -> new NotFoundException("Utente con questa mail: " + email + " non trovato!"));
+	}
+}
